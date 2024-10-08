8c543bc4c573c10eedfa369a7729eecb28018c60
Nico Knünz <nico.knuenz@aon.at>
2024-10-08 22:51:59 +0200

    feat: add buttons to control count of concurrent leds

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -28,16 +28,24 @@ static const char *TAG = "example";
 /* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+#define CYCLE_TIME 10
+#define LED_GPIO 8
+#define LEFT_BUTTON_GPIO 9
+#define RIGHT_BUTTON_GPIO 2
 
-static struct rgb_state rgb_states[25];
+#define LED_COUNT 25
+#define DEFAULT_LED_SIZE 8
 
-#ifdef CONFIG_BLINK_LED_STRIP
+#define DEACTIVATED_SLOT 31
+#define BUTTON_DEBOUNCE 30
+static int button_debounce = BUTTON_DEBOUNCE;
+
+static struct rgb_state rgb_states[LED_COUNT];
 
 static led_strip_handle_t led_strip;
 
-#define select_size 8
-static uint8_t selectedPins[select_size];
+static uint8_t current_led_size = DEFAULT_LED_SIZE;
+static uint8_t selectedPins[LED_COUNT];
 
 static void init_specific_led(uint8_t pin_index)
 {
@@ -48,9 +56,7 @@ static void init_specific_led(uint8_t pin_index)
 
 static bool pin_already_selected(uint8_t selected_pin)
 {
-    uint8_t array_length = sizeof(selectedPins) / sizeof(selectedPins[0]);
-
-    for (int i = 0; i < array_length; i++)
+    for (int i = 0; i < LED_COUNT; i++)
     {
         if (selectedPins[i] == selected_pin)
         {
@@ -61,56 +67,146 @@ static bool pin_already_selected(uint8_t selected_pin)
     return false;
 }
 
-static void pick_new_led(uint8_t index_to_remove)
+static void remove_led_from_index(uint8_t index_to_remove)
 {
-    uint8_t selected_number;
-    bool already_exists = false;
-    do
-    {
-        selected_number = (uint8_t)(esp_random() % 25);
-        already_exists = pin_already_selected(selected_number);
-    } while (already_exists);
-
-    selectedPins[index_to_remove] = selected_number;
-
-    init_specific_led(selected_number);
+    selectedPins[index_to_remove] = DEACTIVATED_SLOT;
 }
 
-static void blink_led(void)
+static void pick_new_led()
 {
-    int i;
-    for (i = 0; i < select_size; i++)
+    int free_index = -1;
+    for (int i = 0; i < LED_COUNT; i++)
     {
-        struct rgb_state *curr_state = &rgb_states[selectedPins[i]];
-
-        led_strip_set_pixel(led_strip, selectedPins[i], curr_state->r, curr_state->g, curr_state->b);
-
-        // start updating just after some debounce time
-        if (curr_state->debounce > 0)
+        if (selectedPins[i] == DEACTIVATED_SLOT)
         {
-            curr_state->debounce = curr_state->debounce - 1;
+            free_index = i;
+            break;
         }
-        else
+    }
+
+    if (free_index != -1)
+    {
+        uint8_t selected_number;
+        bool already_exists = false;
+        do
         {
-            if (curr_state->r > 0)
-            {
-                curr_state->r = curr_state->r - 1;
-            }
+            selected_number = (uint8_t)(esp_random() % 25);
+            already_exists = pin_already_selected(selected_number);
+        } while (already_exists);
 
-            if (curr_state->g > 0)
-            {
-                curr_state->g = curr_state->g - 1;
-            }
+        selectedPins[free_index] = selected_number;
 
-            if (curr_state->b > 0)
-            {
-                curr_state->b = curr_state->b - 1;
-            }
+        init_specific_led(selected_number);
+    }
+}
 
-            if (curr_state->r == 0 && curr_state->g == 0 && curr_state->b == 0)
+static void next_step(void)
+{
+    int curr_button_debounce = button_debounce;
+    led_strip_clear(led_strip);
+
+    int leftButton = gpio_get_level(LEFT_BUTTON_GPIO);
+    int rightButton = gpio_get_level(RIGHT_BUTTON_GPIO);
+
+    // check buttons
+    if (!leftButton || !rightButton || button_debounce != 0)
+    {
+        if (curr_button_debounce == 0)
+        {
+            if (leftButton == 0 && rightButton != 0)
             {
-                pick_new_led(i);
+                // left clicked
+                if (current_led_size > 0)
+                {
+                    current_led_size--;
+                    button_debounce = BUTTON_DEBOUNCE;
+                    curr_button_debounce = button_debounce;
+                }
             }
+            else if (rightButton == 0 && leftButton != 0)
+            {
+                // right clicked
+                if (current_led_size < LED_COUNT)
+                {
+                    current_led_size++;
+                    button_debounce = BUTTON_DEBOUNCE;
+                    curr_button_debounce = button_debounce;
+                }
+            }
+        }
+        if (curr_button_debounce != 0)
+        {
+            button_debounce--;
+
+            for (int i = 0; i < LED_COUNT; i++)
+            {
+                if (i < current_led_size)
+                {
+                    // active
+                    led_strip_set_pixel(led_strip, i, 255, 0, 0);
+                }
+                else
+                {
+                    led_strip_set_pixel(led_strip, i, 0, 0, 0);
+                }
+            }
+        }
+    }
+
+    if (curr_button_debounce == 0)
+    {
+        int still_activated_leds = 0;
+        for (int i = 0; i < LED_COUNT; i++)
+        {
+            int curr_led = selectedPins[i];
+
+            // is the current slot an active led?
+            if (curr_led != DEACTIVATED_SLOT)
+            {
+                // get the state
+                struct rgb_state *curr_state = &rgb_states[curr_led];
+
+                led_strip_set_pixel(led_strip, curr_led, curr_state->r, curr_state->g, curr_state->b);
+
+                // start updating just after some debounce time
+                if (curr_state->debounce > 0)
+                {
+                    curr_state->debounce = curr_state->debounce - 1;
+                    still_activated_leds++;
+                }
+                else
+                {
+                    if (curr_state->r > 0)
+                    {
+                        curr_state->r = curr_state->r - 1;
+                    }
+
+                    if (curr_state->g > 0)
+                    {
+                        curr_state->g = curr_state->g - 1;
+                    }
+
+                    if (curr_state->b > 0)
+                    {
+                        curr_state->b = curr_state->b - 1;
+                    }
+
+                    if (curr_state->r == 0 && curr_state->g == 0 && curr_state->b == 0)
+                    {
+                        remove_led_from_index(i);
+                    }
+                    else
+                    {
+                        still_activated_leds++;
+                    }
+                }
+            }
+        }
+
+        int missing_leds = current_led_size - still_activated_leds;
+        for (int i = 0; i < missing_leds; i++)
+        {
+            pick_new_led();
         }
     }
 
@@ -118,58 +214,44 @@ static void blink_led(void)
     led_strip_refresh(led_strip);
 }
 
-static void configure_led(void)
+static void configure_led_and_buttons(void)
 {
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    ESP_LOGI(TAG, "Configuring LED & buttons!");
     /* LED strip initialization with the GPIO and pixels number*/
     led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
+        .strip_gpio_num = LED_GPIO,
         .max_leds = 25, // at least one LED on board
     };
-#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
+
     led_strip_rmt_config_t rmt_config = {
         .resolution_hz = 10 * 1000 * 1000, // 10MHz
         .flags.with_dma = false,
     };
+
     ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
-    led_strip_spi_config_t spi_config = {
-        .spi_bus = SPI2_HOST,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-#else
-#error "unsupported LED strip backend"
-#endif
+
     /* Set all LED off to clear all pixels */
     led_strip_clear(led_strip);
+
+    gpio_config_t gpioConfigIn = {
+        .pin_bit_mask = (1 << LEFT_BUTTON_GPIO) | (1 << RIGHT_BUTTON_GPIO),
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = true,
+        .pull_down_en = false,
+        .intr_type = GPIO_INTR_DISABLE};
+    gpio_config(&gpioConfigIn);
 }
-
-#elif CONFIG_BLINK_LED_GPIO
-
-static void
-blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
-#else
-#error "unsupported LED type"
-#endif
 
 static void init_led_states(void)
 {
-    for (int i = 0; i < select_size; i++)
+    // write manager for all as deactivated
+    for (int i = 0; i < LED_COUNT; i++)
+    {
+        selectedPins[i] = DEACTIVATED_SLOT;
+    }
+
+    // search random led to start
+    for (int i = 0; i < current_led_size; i++)
     {
         uint8_t selected_number;
         bool already_exists = false;
@@ -183,30 +265,28 @@ static void init_led_states(void)
     }
 
     int debounce = 0;
-    for (int i = 0; i < select_size; i++)
+    for (int i = 0; i < LED_COUNT; i++)
     {
-        init_specific_led(selectedPins[i]);
-        rgb_states[selectedPins[i]].debounce = debounce;
-        debounce += 30;
+        if (selectedPins[i] != DEACTIVATED_SLOT)
+        {
+            init_specific_led(selectedPins[i]);
+            rgb_states[selectedPins[i]].debounce = debounce;
+            debounce += 30;
+        }
     }
 }
 
 void app_main(void)
 {
-
-    /* Configure the peripheral according to the LED type */
-    configure_led();
-
-    led_strip_clear(led_strip);
-
-    ESP_LOGI(TAG, "Init of LED-States!");
+    ESP_LOGI(TAG, "Start of programm!");
+    configure_led_and_buttons();
+    ESP_LOGI(TAG, "Configured LED & buttons! Start random init of LED-States!");
     init_led_states();
-    ESP_LOGI(TAG, "Finished init of LED-States!");
+    ESP_LOGI(TAG, "Finished init of LEDS. Start Program!");
 
     while (1)
     {
-        // ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
+        next_step();
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
